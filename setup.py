ab166a70b83e72c43ca0ae05feaf5e968be5cecf
Prathik Jain <prathik.jain.gn@gmail.com>
2024-06-22 23:26:43 +0530

    Updated Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Prathik Jain',
+    author_email= 'prathik.jain.gn@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
